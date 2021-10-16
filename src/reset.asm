373973f5bb3582db0b63aefab3e4a1246b2f5544
Sébastien Roccaserra <sroccaserra@yahoo.com>
2021-10-16 23:59:31 +0200

    Add background

--- src/reset.asm
+++ src/reset.asm
@@ -13,8 +13,24 @@
   LDX #$00
   STX PPUCTRL       ; turn off non maskable interrupts (NMI)
   STX PPUMASK       ; disable rendering
+
 vblankwait:
   BIT PPUSTATUS
   BPL vblankwait    ; loop waiting for vblank
+
+  LDX #$00
+  LDA #$ff
+clear_oam:
+  STA $0200,X       ; set all sprite y-positions off the screen
+  INX
+  INX
+  INX
+  INX
+  BNE clear_oam
+
+vblankwait2:
+  BIT PPUSTATUS
+  BPL vblankwait2
+
   JMP main
 .endproc
